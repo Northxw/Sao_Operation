e145b01afaee0c152641c17af303ff88dfea7fbb
Northxw <northxw@163.com>
2018-10-19 01:00:13 +0800

    Modified send_sms.py file

--- 01_Send Messages/send_sms.py
+++ 01_Send Messages/send_sms.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
 from twilio.rest import Client
 
-account = 'AC7a5ec45b5b97ed9b4338125b90c7e6aa'
-token = 'fb17167e075cb2577ca1dbd0df31076d'
+# The value of the following authentication information can be found in your twilio account.
+account = 'account_sid'
+token = 'auth_token'
 client = Client(account, token)
 
 message = client.messages.create(
-    to='+8613201723305',
-    from_='+1 541 403 9876',
-    body="This is a FoLeng! Haha..."
+    to='+86132****3305',        # Your phone number
+    from_='+1 541 403 9876',    # Your twilio number
+    body="This is a FoLeng! Haha..."    # SMS content to be sent
 )
