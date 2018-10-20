89db579af570a56db006ab448be47d661fd996cc
Northxw <northxw@163.com>
2018-10-20 16:24:45 +0800

    Re-upload project

---
+++ 01-Send_an_SMS/utils/config.py
@@ -0,0 +1,28 @@
+# YOUR PHONE NUMBER
+YOUR_PHONE_NUMBER = 'Your Phone Number'    # 测试用
+# MESSAGES_BODY
+BODY = "I Love You!"
+
+# TWILIO NUMBER
+TWILIO_NUMBER = "+1 541 403 9876"
+# ACCOUNT SID
+TWILIO_ACCOUNT_SID = 'AC7a5ec45b5b****9b4338125b90c7e6aa'   # Twilio个人页面查看自己的SID并更换
+# AUTH TOKEN
+TWILIO_AUTH_TOKEN = 'fb****67e075********1dbd0df****6d'     # Twilio个人页面查看自己的TOKEN并更换
+
+# SPECIAL DAY
+DATE = {
+    'Dad': '2019-06-16',
+    'Mom': '2019-05-12',
+    'Baby': '2019-02-14',
+    'Me': '2018-10-19'      # 测试用的日期，请更换至你当下的日期时间
+}
+
+# PHONE NUMBER
+CONTACT_PHONE_NUMBER = {
+    'Dad': '+8618888888888',
+    'Mom': '+8618888888888',
+    'Baby': '+8618888888888',
+    'Me': '+86132***3305'   # 测试用，请填写自己再Twilio注册时的手机号码
+}
+
