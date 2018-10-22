8e3a058fc6224d8a9083a77191874a654f114a60
Northxw <northxw@163.com>
2018-10-22 23:59:25 +0800

    Modified

--- 02-Open_WeChat/Open_WeChat.py
+++ 02-Open_WeChat/Open_WeChat.py
@@ -10,23 +10,23 @@ server = 'http://localhost:4723/wd/hub'
 
 # 配置 Desired Capabilities (启动APP的参数)
 desired_caps = {
-    'platformName': 'Android',
-    'deviceName': 'vivo_X7',
-    'appPackage': 'com.tencent.mm',
-    'appActivity': '.ui.LauncherUI'
+    'platformName': 'Android',  # 设备类型，分为Android或iOS
+    'deviceName': 'vivo_X7',    # 设备名称
+    'appPackage': 'com.tencent.mm',     # App包名
+    'appActivity': '.ui.LauncherUI'     # 入口
 }
 
 # 新建 Session(与点击Appium内置的Start Session按钮的功能相同)
 driver = webdriver.Remote(server, desired_caps)
 
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 30)    # 设置页面加载等待,保证所有节点完全加载出来
 login = wait.until(EC.presence_of_element_located((By.ID, 'com.tencent.mm:id/drp')))    # 微信欢迎页面登录按钮
 login.click()
 phone_password = wait.until(EC.presence_of_element_located((By.ID, 'com.tencent.mm:id/ji')))     # 输入手机号的文本栏
-phone_password.set_text('132****3305')
+phone_password.set_text('***********')
 next_login = wait.until(EC.presence_of_element_located((By.ID, 'com.tencent.mm:id/ast')))     # 下一步
 next_login.click()
-phone_password.send_keys('A******8')    # 密码输入框的ID和手机号输入框的ID相同,所以不重复获取
+phone_password.send_keys('********')    # 密码输入框的ID和手机号输入框的ID相同,所以不重复获取
 
 try:
     next_login.click()          # 最后的登录按钮ID与欢迎界面的登录按钮的ID相同,不重复获取
