6091d04e274c5c8dd31ed963517103dac5ee7271
Northxw <northxw@163.com>
2018-10-22 23:44:48 +0800

    Add a comment to send_sms file

--- 01-Send_an_SMS/send_sms.py
+++ 01-Send_an_SMS/send_sms.py
@@ -19,7 +19,7 @@ class Sms(object):
         """
         self.account = account
         self.token = token
-        self.client = Client(self.account, self.token)
+        self.client = Client(self.account, self.token)  # 初始化Client对象
 
     def parse_date(self):
         """
@@ -34,7 +34,7 @@ class Sms(object):
         根据日期向特定人发送问候留言
         :return: None
         """
-        target_date = self.parse_date()
+        target_date = self.parse_date()     # 目标日期(特定日期,比如情人节)
         for name, date in DATE.items():
             if date == target_date:                           # 遍历DATE字典,判断目标日期是否包含在DATE.keys()
                 body = name + ', ' + BODY                     # 根据节日不同, 发送内容不同的短信
@@ -45,12 +45,12 @@ class Sms(object):
                         from_=TWILIO_NUMBER,
                         body=body)
                 except Exception as e:
-                    _ = e
+                    _ = e                   # 接收异常(_符号总是记录最近的改变)
                     # print(format_exc())
                     print('Failed!')
                 else:
                     print('Successfully!')
 
 if __name__ == '__main__':
-    sms = Sms()
-    sms.send()
+    sms = Sms()     # 创建Sms类对象
+    sms.send()      # 发送信息
